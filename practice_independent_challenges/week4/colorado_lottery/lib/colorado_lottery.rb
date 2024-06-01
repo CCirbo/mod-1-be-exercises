c36052a4c08ceb0fb86b9af1b63f6a0090fe246d
Candice Cirbo <thecirbos@icloud.com>
2024-06-01 17:09:47 -0600

    added methods for colorado lottery

---
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -0,0 +1,35 @@
+class ColoradoLottery
+    attr_reader :registered_contestants,
+                :winners,
+                :current_contestants
+    def initialize
+     @registered_contestants = {}
+     @winners = []
+     @current_contestants = {}
+    end
+
+    def interested_and_18?(contestant, game)
+        contestant.age >= 18 && contestant.game_interests.include?(game.name)
+     
+        # if contestant is both interested in a given name 
+        # and 18 and over
+        # return true
+        # else false
+        # binding.pry
+    end
+
+    def can_register?(contestant, game)
+        # binding.pry
+        # if contestant is interested, 18 and lives in CO, they can get any game
+        # so don't need to test for that, we only need to look at who is not
+        # a resident of colorado
+        # if a game is national, they can register
+        # then can register for game
+        # game.national_drawing? == false it is for CO residents only
+        if game.national_drawing? == false && contestant.state_of_residence != "CO" 
+            false
+        else
+         interested_and_18?(contestant, game) 
+        end
+    end
+end   
--- practice_independent_challenges/week4/colorado_lottery/spec/colorado_lottery_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/colorado_lottery_spec.rb
@@ -1,6 +1,6 @@
 require "./lib/contestant"
 require "./lib/game"
-require '.lib/colorado_lottery'
+require "./lib/colorado_lottery"
 require "pry"
 require 'rspec'
 
@@ -40,45 +40,53 @@ RSpec.configure do |config|
     end
 
     it 'it exists' do
-        expect(@lottery).to be be_a(ColoradoLottery)
+        expect(@lottery).to be_an_instance_of(ColoradoLottery)
     end
 
-    xit "has no registered_contestants by default" do
+    it "has no registered_contestants by default" do
         expect(@lottery.registered_contestants).to eq({})
     end
 
-    xit 'has no lottery_winners by default' do
+    it 'has no lottery_winners by default' do
         expect(@lottery.winners).to eq([])
     end
 
-    xit 'has no lottery_current_winners by default' do
+    it 'has no lottery_current_winners by default' do
         expect(@lottery.current_contestants).to eq({})
     end
 
-    xit "can tell if a contestant is interested in a game and over 18" do
+    it "can tell if a contestant is interested in a game and over 18" do
         @alexander.add_game_interest('Pick 4')
         # @alexander.add_game_interest('Mega Millions')
-        expect(@lottery.interested_and_18?(alexander, pick_4)).to eq(true)
-        expect(@lottery.interested_and_18?(alexander, cash_5)).to eq(false)
+        expect(@lottery.interested_and_18?(@alexander, @pick_4)).to eq(true)
+        expect(@lottery.interested_and_18?(@alexander, @cash_5)).to eq(false)
       
         # @winston.add_game_interest('Mega Millions')
 
         @benjamin.add_game_interest('Mega Millions')
-        expect(@lottery.interested_and_18?(benjamin, mega_millions)).to eq(false)
-        expect(@lottery.interested_and_18?(benjamin, cash_5)).to eq(false)
+        expect(@lottery.interested_and_18?(@benjamin, @mega_millions)).to eq(false)
+        expect(@lottery.interested_and_18?(@benjamin, @cash_5)).to eq(false)
         # @winston.add_game_interest('Cash 5')
     end
 
-    xit "can register? contestants if interested, 18 and is in CO" do
-        @alexander.add_game_interest('Pick 4')        
-        expect(@lottery.can_register?(alexander, pick_4)).to eq(true)
-        expect(@lottery.can_register?(alexander, cash_5)).to eq(false)
+    it "can register? contestants if interested, 18 and is in CO" do
+        @alexander.add_game_interest('Pick 4')  #we need a string back 
+        @alexander.add_game_interest("Mega Millions")  #we need a string back 
+        expect(@lottery.can_register?(@alexander, @pick_4)).to eq(true)
+        expect(@lottery.can_register?(@alexander, @mega_millions)).to eq(true)
+        expect(@lottery.can_register?(@alexander, @cash_5)).to eq(false)
 
         @frederick.add_game_interest('Mega Millions')
-        expect(@lottery.can_register?(frederick, mega_millions)).to eq(true)
-        expect(@lottery.can_register?(frederick, cash_5)).to eq(false)
+        @frederick.add_game_interest('Cash 5')
+        expect(@lottery.can_register?(@frederick, @mega_millions)).to eq(true)
+        expect(@lottery.can_register?(@frederick, @cash_5)).to eq(false)
+
+        @winston.add_game_interest('Cash 5')
+        @winston.add_game_interest('Mega Millions')
+        expect(@lottery.can_register?(@winston, @cash_5)).to eq(true)
+        expect(@lottery.can_register?(@winston, @cash_5)).to eq(true)
 
         @benjamin.add_game_interest('Mega Millions')
-        expect(@lottery.can_register?(benjamin, mega_millions)).to eq(false)
+        expect(@lottery.can_register?(@benjamin, @mega_millions)).to eq(false)
     end
 end
